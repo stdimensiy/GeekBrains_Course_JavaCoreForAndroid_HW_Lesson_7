dd5eb50013a5b89fe55f7e671e209d7d74e43c9d
StDimensiy <stdimensiy@yandex.ru>
2021-02-08 14:18:18 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson7.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.JavaCoreForAndroid;
+
+/**
+ * Сourse: java core for android
+ * Faculty of Geek University Android Development
+ *
+ * @Author Student Dmitry Veremeenko aka StDimensiy
+ * Group 24.12.2020
+ *
+ * HomeWork for lesson1
+ * Created 08.02.2021
+ * v1.0
+ */
+public class Lesson7 {
+}
