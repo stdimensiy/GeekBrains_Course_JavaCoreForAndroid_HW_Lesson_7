d1486ab8d1aebe86923269773b45118a3cf69af8
StDimensiy <stdimensiy@yandex.ru>
2021-02-10 16:40:40 +0300

    Задачи №1 - выполнена.

---
+++ src/main/java/ru/geekbrains/JavaCoreForAndroid/Human.java
@@ -0,0 +1,32 @@
+package ru.geekbrains.JavaCoreForAndroid;
+
+/**
+ * Сourse: java core for android
+ * Faculty of Geek University Android Development
+ *
+ * @Author Student Dmitry Veremeenko aka StDimensiy
+ * Group 24.12.2020
+ *
+ * HomeWork for lesson 7
+ * Created 10.02.2021
+ * v1.0
+ */
+public class Human {
+    private String name;
+    private double maxHeight;      // максимальная высота на на которую может подняться участник
+    private int maxRun;            // предельное расстояние непрерывного бега участника
+
+    public Human(String name, double maxHeight, int maxRun) {
+        this.name = name;
+        this.maxHeight = maxHeight;
+        this.maxRun = maxRun;
+    }
+
+    public void run(int distance){
+        System.out.println("Человек пробежал " + distance);
+    }
+
+    public void jump(double height){
+        System.out.println("Человек подпрыгнул на " + height +"м.");
+    }
+}
